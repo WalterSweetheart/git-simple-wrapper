965f1028744309028dd180cff06ce380f048fa4c
Walter Sweetheart <one.sweet.walter@gmail.com>
2021-09-29 13:37:34 +0500

    1.0.0
    break: new api

--- gsw.sh
+++ gsw.sh
@@ -2,103 +2,97 @@
 
 if [ -n "$1" ]
 then
-    if [[ "$1" == "set" ]] && [[ "$3" == "to" ]]
+    if [ "$1" = "set" ] && [ "$3" = "to" ]
     then
         eval "git config --global \"$2\" \"$4\""
         exit 0
-    elif [[ "$1" == "show" ]]
+    elif [ "$1" = "show" ]
     then
-        if [[ "$2" == "settings" ]]
-        then
-            eval "git config --list"
-            exit 0
-        elif [[ "$2" == "history" ]]
+        if [ "$2" = "history" ]
         then
             eval "git log"
             exit 0
-        elif [[ -n "$2" ]]
+        elif [ "$2" = "settings" ]
+        then
+            eval "git config --list"
+            exit 0
+        elif [ -n "$2" ]
         then
             eval "git config \"$2\""
             exit 0
         fi
-    elif [[ "$1" == "init" ]]
+    elif [ "$1" = "create" ] && [ "$2" = "repo" ]
     then
-        eval "git init"
-        eval "git add -A"
-        eval "git commit -m \"0.0.0\" -m \"Initial commit\""
-        exit 0
-    elif [[ "$1" == "add" ]] && [[ -n "$3" ]]
-    then
-        VERSION="$(git log -1 --pretty=%B | head -n 1)"
-        NEW_VERSION=${VERSION%.*}.$((${VERSION##*.}+1))
-        if [[ "$2" == "feat"     ]] ||
-           [[ "$2" == "fix"      ]] ||
-           [[ "$2" == "docs"     ]] ||
-           [[ "$2" == "style"    ]] ||
-           [[ "$2" == "refactor" ]] ||
-           [[ "$2" == "test"     ]] ||
-           [[ "$2" == "chore"    ]]
+        if [ "$3" = "locally" ]
         then
+            eval "git init"
             eval "git add -A"
-            eval "git commit -m \"$NEW_VERSION\" -m \"$2: $3\""
+            eval "git commit -m \"0.0.0\" -m \"break: initial commit\""
+            exit 0
+        elif [ "$3" = "at" ] && [ -n "$4" ]
+        then
+            eval "git init"
+            eval "git add -A"
+            eval "git commit -m \"0.0.0\" -m \"break: initnal commit\""
+            eval "git remote add origin $4"
+            eval "git push --set-upstream origin main --force"
+            exit 0
+        elif [ "$3" = "from" ] && [ -n "$4" ]
+        then
+            eval "rm -rf * .*"
+            eval "git clone $4 ."
             exit 0
         fi
-    elif [[ "$1" == "next" ]] && [[ -n "$2" ]] && [[ -n "$3" ]]
+    elif [ "$1" = "connect" ] && [ "$2" = "to" ] && [ -n "$3" ]
     then
-        VERSION="$(git log -1 --pretty=%B | head -n 1)"
-        NEW_VERSION_MAJOR=${VERSION%%.*}
-        NEW_VERSION_MINOR=${VERSION%.*}
-        NEW_VERSION_MINOR=${NEW_VERSION_MINOR#*.}
-        NEW_VERSION_SUB=${VERSION##*.}
-        if [[ "$2" == "major" ]]
+        eval "git remote remove origin"
+        eval "git remote add origin $3"
+        eval "git push --set-upstream origin main --force"
+    elif [ "$1" = "add" ] &&
+         ( [ "$2" = "break" ] ||
+           [ "$2" = "feat"  ] ||
+           [ "$2" = "fix"   ] ||
+           [ "$2" = "docs"  ] ) &&
+         [ -n "$3" ]
+    then
+        VERSION=$( git log -1 --pretty=%B | head -n 1 )
+        VERSION_MAJOR=${VERSION%%.*}
+        VERSION_MINOR=${VERSION%.*}
+        VERSION_MINOR=${VERSION_MINOR#*.}
+        VERSION_SUB=${VERSION##*.}
+        if [ "$2" = "break" ]
         then
-            ((NEW_VERSION_MAJOR += 1))
-            NEW_VERSION_MINOR=0
-            NEW_VERSION_SUB=0
-        elif [[ "$2" == "minor" ]]
+            VERSION_MAJOR=$(( $VERSION_MAJOR + 1 ))
+            VERSION_MINOR=0
+            VERSION_SUB=0
+        elif [ "$2" = "feat" ]
         then
-            ((NEW_VERSION_MINOR += 1))
-            NEW_VERSION_SUB=0
-        elif [[ "$2" == "sub" ]]
+            VERSION_MINOR=$(( $VERSION_MINOR + 1 ))
+            VERSION_SUB=0
+        elif [ "$2" = "fix" ]
         then
-            ((NEW_VERSION_SUB += 1))
-        else
-            echo "Error: unknown command" >&2
-            exit 1
+            VERSION_SUB=$(( $VERSION_SUB + 1 ))
         fi
+        VERSION="$VERSION_MAJOR.$VERSION_MINOR.$VERSION_SUB"
         eval "git add -A"
-        eval "git commit -m \"$NEW_VERSION_MAJOR.$NEW_VERSION_MINOR.$NEW_VERSION_SUB\" -m \"$3\" --allow-empty"
-        exit 0
-    elif [[ "$1" == "connect" ]] && [[ "$2" == "to" ]] && [[ -n "$3" ]] && [[ "$4" == "as" ]] && [[ -n "$5" ]]
-    then
-        eval "git remote add \"$5\" \"$3\""
-        eval "git push --set-upstream origin main -- force"
-        exit 0
-    elif [[ "$1" == "push" ]]
-    then
-        eval "git push"
-        exit 0
+        eval "git commit -m \"$VERSION\" -m \"$2: $3\""
     fi
 fi
 
-echo "GSW -- git simple wrapper by WalterSweetheart"                                                    >&2
-echo "Usage: gsw [command]"                                                                             >&2
-echo "           set <setting> to <value>  -- sets setting to value in git config globally"             >&2
-echo "           show [what]"                                                                           >&2
-echo "                settings  -- shows settings list"                                                 >&2
-echo "                <setting>  -- shows setting"                                                      >&2
-echo "           add [type]"                                                                            >&2
-echo "               feat     <message>  -- adds sub feat commit"                                       >&2
-echo "               fix      <message>  -- adds sub fix commit"                                        >&2
-echo "               docs     <message>  -- adds sub docs commit"                                       >&2
-echo "               style    <message>  -- adds sub style commit"                                      >&2
-echo "               refactor <message>  -- adds sub refactor commit"                                   >&2
-echo "               test     <message>  -- adds sub test commit"                                       >&2
-echo "               chore    <message>  -- adds sub chore commit"                                      >&2
-echo "           next [type]"                                                                           >&2
-echo "                major <message>  -- increases major version and makes commit with message"        >&2
-echo "                minor <message>  -- increases minor version and makes commit with message"        >&2
-echo "                sub   <message>  -- increases sub version and makes commit with message"          >&2
-echo "           connect to <remote> as <name>  -- connects to remote as name"                          >&2
-echo "           push  -- push changes to origin"                                                       >&2
-exit 1
+echo "GSW -- git simple wrapper by WalterSweetheart"                                                                >&2
+echo "Usage: gsw"                                                                                                   >&2
+echo "           set <setting> to <value>  -- sets setting to value in git config"                                  >&2
+echo "           show"                                                                                              >&2
+echo "                history    -- shows git log"                                                                  >&2
+echo "                settings   -- shows setting list"                                                             >&2
+echo "                <setting>  -- shows setting value"                                                            >&2
+echo "           create repo"                                                                                       >&2
+echo "                       locally  -- creates new git repo and initial commit"                                   >&2
+echo "                       at <link>  -- creates new repo, initial commit, connects to repo and pushes with force">&2
+echo "                       from <link>  -- erases all files and clones repo"                                      >&2
+echo "           connect to <link>  -- connect to repo and pushes with force"                                       >&2
+echo "           add"                                                                                               >&2
+echo "               break <message>  -- makes major break commit and pushes it"                                    >&2
+echo "               feat  <message>  -- makes minor feat commit and pushes it"                                     >&2
+echo "               fix   <message>  -- makes sub fix commit and pushes it"                                        >&2
+echo "               docs  <message>  -- makes docs commit and pushes it"                                           >&2
